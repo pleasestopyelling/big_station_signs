782c771b4200b23433d25eaddc599365ec0e0110
pleasestopyelling <cerachew@gmail.com>
2025-11-23 21:57:37 +0000

    added signs to tags

--- src/main/java/net/cutecharm/bigstationsigns/datagen/ModBlockTagProvider.java
+++ src/main/java/net/cutecharm/bigstationsigns/datagen/ModBlockTagProvider.java
@@ -1,6 +1,7 @@
 package net.cutecharm.bigstationsigns.datagen;
 
 import net.cutecharm.bigstationsigns.block.ModBlocks;
+import net.cutecharm.bigstationsigns.tags.ModBlockTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
@@ -13,11 +14,10 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
         super(output, registriesFuture);
     }
 
+
     //blocks to be mined with pickaxe
     @Override
     protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
-        getOrCreateTagBuilder(BlockTags.AXE_MINEABLE)
-                .add(ModBlocks.SIGNING_TABLE);
         getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
 
                 .add(ModBlocks.WHITE_BIG_STATION_SIGN)
@@ -51,7 +51,40 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
                 .add(ModBlocks.CRIMSON_BIG_STATION_SIGN)
                 .add(ModBlocks.WARPED_BIG_STATION_SIGN)
                 .add(ModBlocks.CHERRY_BIG_STATION_SIGN)
-                .add(ModBlocks.MANGROVE_BIG_STATION_SIGN);
+                .add(ModBlocks.MANGROVE_BIG_STATION_SIGN)
+                .add(ModBlocks.SIGNING_TABLE);
+
+        getOrCreateTagBuilder(ModBlockTags.BIG_STATION_SIGNS)
+                .add(ModBlocks.OAK_BIG_STATION_SIGN)
+                .add(ModBlocks.BIRCH_BIG_STATION_SIGN)
+                .add(ModBlocks.JUNGLE_BIG_STATION_SIGN)
+                .add(ModBlocks.SPRUCE_BIG_STATION_SIGN)
+                .add(ModBlocks.ACACIA_BIG_STATION_SIGN)
+                .add(ModBlocks.DARK_OAK_BIG_STATION_SIGN)
+                .add(ModBlocks.BAMBOO_BIG_STATION_SIGN)
+                .add(ModBlocks.CRIMSON_BIG_STATION_SIGN)
+                .add(ModBlocks.WARPED_BIG_STATION_SIGN)
+                .add(ModBlocks.CHERRY_BIG_STATION_SIGN)
+                .add(ModBlocks.MANGROVE_BIG_STATION_SIGN)
+                .add(ModBlocks.WHITE_BIG_STATION_SIGN)
+                .add(ModBlocks.LIGHT_GRAY_BIG_STATION_SIGN)
+                .add(ModBlocks.GRAY_BIG_STATION_SIGN)
+                .add(ModBlocks.GRAY_BIG_STATION_SIGN_B)
+                .add(ModBlocks.BLACK_BIG_STATION_SIGN)
+                .add(ModBlocks.BROWN_BIG_STATION_SIGN)
+                .add(ModBlocks.RED_BIG_STATION_SIGN)
+                .add(ModBlocks.ORANGE_BIG_STATION_SIGN)
+                .add(ModBlocks.YELLOW_BIG_STATION_SIGN)
+                .add(ModBlocks.LIME_BIG_STATION_SIGN)
+                .add(ModBlocks.GREEN_BIG_STATION_SIGN)
+                .add(ModBlocks.CYAN_BIG_STATION_SIGN)
+                .add(ModBlocks.LIGHT_BLUE_BIG_STATION_SIGN)
+                .add(ModBlocks.BLUE_BIG_STATION_SIGN)
+                .add(ModBlocks.PURPLE_BIG_STATION_SIGN)
+                .add(ModBlocks.PURPLE_BIG_STATION_SIGN_B)
+                .add(ModBlocks.MAGENTA_BIG_STATION_SIGN)
+                .add(ModBlocks.PINK_BIG_STATION_SIGN)
+                .add(ModBlocks.CERULEAN_BIG_STATION_SIGN);
 
     }
 }
